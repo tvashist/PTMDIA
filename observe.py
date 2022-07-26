6c6c3a80ba492d24be7e6a67a4d23a93ccade5e1
CHARLES\tvashist <tvashist@broadinstitute.org>
2022-07-26 14:22:54 -0400

    Changed the way I am counting phosphopeptides and updated user friendliness

--- observe.py
+++ observe.py
@@ -4,12 +4,12 @@ import math
 from statistics import mean,stdev,mode
 
 #Get a summary of the number of phosphopeptide and phosphosite counts in the runs without spiked peptides
-spectronaut = pd.read_csv('20220714_100348_PTMDIAProject_Pro_PhosphoBG_Report.tsv', delimiter='\t')
-# head = spectronaut[:50]
-# head.to_csv('ShortPhoshpoBGSCP.tsv', sep = '\t')
+descriptor = 'Pro_PhosphoBG_NoSpike'            #How you want your replicates to be annotated
+spectronaut = pd.read_csv('C:/Users/tvashist/PycharmProjects/PTMDIA_Project/20220714_100348_PTMDIAProject_Pro_PhosphoBG_Report.tsv', delimiter='\t')     #Entire data frame
 
 nospike = spectronaut.loc[spectronaut['R.FileName'].str.contains('NoSpike')]        #Only look at the entries that are from the no spike runs
-reps = set(nospike['R.FileName'])
+reps = list(set(nospike['R.FileName']))                                                  #Unique runs in file, so all three reps of no spike runs
+
 
 def count_sites(df):
 
@@ -26,16 +26,40 @@ def count_sites(df):
             if site_info not in sites:
                 sites.append(site_info)
 
-    return(len(set(sites)))
+    return(len(set(sites)))                                 #Returns number of unique phosphosites based on location on protein sequence
 
-for run in reps:
-    phosphos = nospike.loc[nospike['R.FileName'] == run]
-    phosphos = phosphos.loc[phosphos['EG.IntPIMID'].str.contains('[+80]')]
-    print(run[-4:])
-    unique_phosphopeptides = len(set(phosphos['EG.IntPIMID']))
-    phosphosites = count_sites(phosphos)
-    print("Number of unique phosphopeptides: " + str(unique_phosphopeptides))
-    print("Number of phosphosites: " + str(phosphosites))
+phospho_summary = {}
+phospho_summary['Run'] = []
+phospho_summary['Phosphopeptides'] = []
+phospho_summary['Phosphosites'] = []
+
+
+print("here before loop!")
+for rep in range(0, len(reps)):
+    unique_phospho_status = []
+
+    run = reps[rep]
+    run_id = descriptor +"_"+ "0" + str(rep)                                        #Run descriptor that will go in data frame
+
+    phosphos = nospike.loc[nospike['R.FileName'] == run]                            #Subset to just one rep
+    phosphos = phosphos.loc[phosphos['EG.IntPIMID'].str.contains('[+80]')]          #All entries that contain phosphorylated amino acids
+    for x in phosphos['EG.IntPIMID']:
+        new = x.replace('[+16]','').replace('[+57]','')                             #Remove annotations for oxidized methionine and carbamidomethylation
+        unique_phospho_status.append(new)
+    print("I just changed all the mod annotations")
+
+    unique_phosphopeptides = len(set(unique_phospho_status))                        #Sequences with unique phosphorylation status
+    phosphosites = count_sites(phosphos)                                            #Unique phosphosites based on location on protein sequence
+
+    phospho_summary['Run'].append(run_id)
+    phospho_summary['Phosphopeptides'].append(unique_phosphopeptides)
+    phospho_summary['Phosphosites'].append(phosphosites)
+
+
+report = pd.DataFrame.from_dict(phospho_summary)
+report.to_csv('C:/Users/tvashist/PycharmProjects/PTMDIA_Project/PhosphoBG_Curve/Pro_NoSpike_Summary/Summary.tsv', sep = '\t')
+
+
 
 
 
